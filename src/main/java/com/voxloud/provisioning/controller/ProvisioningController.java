8954e13917e3cbf9bfa2a4f93ea86c1624ea6bf8
risyandi <risyandi.94@gmail.com>
2025-04-12 21:41:14 +0700

    fix: adding hello

--- src/main/java/com/voxloud/provisioning/controller/ProvisioningController.java
+++ src/main/java/com/voxloud/provisioning/controller/ProvisioningController.java
@@ -2,10 +2,15 @@ package com.voxloud.provisioning.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/api/v1")
 public class ProvisioningController {
 
-    // TODO Implement controller method
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hi, Greetings from Spring Boot.!";
+    }
+
 }
